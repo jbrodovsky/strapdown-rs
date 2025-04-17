08609daa7a19c6c81df512e54646fc1caa3a9547
James Brodovsky <james.brodovsky@gmail.com>
2025-04-17 10:05:51 -0400

    renamed core library to just strapdown

--- libstrapdown/src/lib.rs
+++ libstrapdown/src/lib.rs
@@ -2,5 +2,5 @@ use std::os::raw::c_double;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn ins_add(a: c_double, b: c_double) -> c_double {
-    strapdown_core::add(a, b)
+    strapdown::add(a, b)
 }
--- strapdown-py/src/lib.rs
+++ strapdown-py/src/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 fn add(a: f64, b: f64) -> f64 {
-    strapdown_core::add(a, b)
+    strapdown::add(a, b)
 }
 
 #[pymodule]
